e34ff389c74e1d52049680dfb25762c6fc4bd217
Avinash Singh Chauhan <30377488+avinashchauhan17n@users.noreply.github.com>
2017-07-24 23:33:28 +0530

    Update main.c

--- main.c
+++ main.c
@@ -5,24 +5,23 @@
 int main(void){
 
 /***************local variables declaration start here****************************************************/
-
 	FILE *filetowriteDATA;
 	int block_size,block_count;
-	int retVALUE;
-	char *buffer;
+ 	int retVALUE;
+ 	char *buffer;
 
 /***************local variables declaration ends here*****************************************************/
 
 /***************User Inputs for block_size and block count starts*****************************************/
 	printf("Enter the Block size\n");
-    scanf("%d",&block_size);
+    	scanf("%d",&block_size);
 	printf("Enter the Block Count\n");
-    scanf("%d",&block_count);
-
-    if ( (buffer = (char *)malloc(block_size*sizeof(char))) == NULL){
-        printf("Error in Allocating desired buffer size\n");
+    	scanf("%d",&block_count);
+	
+    	if ( (buffer = (char *)malloc(block_size*sizeof(char))) == NULL){
+        	printf("Error in Allocating desired buffer size\n");
         EXIT_FAILURE;
-    }
+    	}
 /***************User Inputs for block_size and block count ends******************************************/
 
 	if ((filetowriteDATA=fopen(SOURCE,"w"))==NULL){
@@ -35,8 +34,7 @@ int main(void){
                printf("FIle Write Error\n");
                EXIT_FAILURE;
               }
-
 	       block_count--;
-          }
+       	}
 	EXIT_SUCCESS;
 }
