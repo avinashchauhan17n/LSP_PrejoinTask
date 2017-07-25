25a6045e517e18254e6dd516440301b83b96d0a7
Avinash Singh Chauhan <30377488+avinashchauhan17n@users.noreply.github.com>
2017-07-25 11:47:11 +0530

    Update 4_b_RAW_I-O.c

--- 4_b_RAW_I-O.c
+++ 4_b_RAW_I-O.c
@@ -3,47 +3,53 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <time.h>
+#include <string.h>
 #define FILETOWRITE "data.txt"
 #define MODE O_CREAT|O_WRONLY
-#define PERMISSION 644
+#define PERMISSION 644          //rw-r-r
 int main(void) {
         /***************local variables declaration ends here*****************************************************/
-        int fdWRITE,retVALUE;
+        int fdWrite,retValue;
         char *buffer;
-        int blockSIZE, blockCOUNT;
+        int blockSize, blockCount;
         /***************local variables declaration ends here*****************************************************/
         
         /***************User Inputs for block_size and block count starts*****************************************/
         printf("Enter the Block size\n");
-        scanf("%d",&blockSIZE);
+        scanf("%d",&blockSize);
         printf("Enter the Block Count\n");
-        scanf("%d",&blockCOUNT);
+        scanf("%d",&blockCount);
 
-        if ( (buffer = (char *)malloc(blockSIZE*sizeof(char))) == NULL){
+        if ( (buffer = (char *)malloc(blockSize*sizeof(char))) == NULL){
                 printf("Error in Allocating desired buffer size\n");
                 EXIT_FAILURE;
         }
         /***************User Inputs for block_size and block count ends******************************************/
 
-        if( (buffer  = calloc(blockSIZE,(sizeof(char)))) == NULL) {
-                printf("Memory not Allocated:error");
+        if( (buffer  = calloc(blockSize,(sizeof(char)))) == NULL) {
+                printf("Memory not Allocated:error\n");
                 EXIT_FAILURE;
         }
         
-        if( (fdWRITE = open(FILETOWRITE,MODE,PERMISSION)) == -1) {
-                printf("Open Error= %d\n", errno);
-                perror("FILETOWRITE");
+        if( (fdWrite = open(FILETOWRITE,MODE,PERMISSION)) == -1) {
+                printf("Error %s",strerror(errno));
                 EXIT_FAILURE;
         }
         /***************Writing buffer with size Blocksize given by user for Bockcount number of times**********/
-        while( blockCOUNT > 0) {
-                if( (retVALUE = write(fdWRITE, buffer, blockSIZE)) == -1) {
-                        printf("Error:Write = %d\n", errno);
-                        perror(FILETOWRITE);
-                        EXIT_FAILURE;
+        clock_t start= clock();                  // starting the clock call to count the event
+        while( blockCount > 0) {
+                if( (retValue = write(fdWrite, buffer, blockSize)) == -1) {
+                     printf("Error:Write = %s\n",strerror(errno)); 
+                     EXIT_FAILURE;
                 }
-                blockCOUNT--;
+        	blockCount--;
         }
+	clock_t end=clock();                   //clock event ends here
+	/***************Writing buffer operation ends here*****************************************************/
 
+        double timeDiff=(double)(end-start);   
+        timeDiff/=CLOCKS_PER_SEC;              // taking diff of operation for buff write in terms of clocks/sec
+        printf("Time taken for write cycle: %lf\n",timeDiff);
         EXIT_SUCCESS;
 }
