fa7358413bb5b7f08ae882fffdea8713e95e87ea
Avinash Singh Chauhan <30377488+avinashchauhan17n@users.noreply.github.com>
2017-07-27 17:22:15 +0530

    Update 4_b_RAW_I-O.c

--- 4_b_RAW_I-O.c
+++ 4_b_RAW_I-O.c
@@ -8,51 +8,51 @@
 #define FILETOWRITE "data.txt"
 #define MODE O_CREAT|O_WRONLY
 #define PERMISSION 644          //rw-r-r
+
 int main(void) {
-        /***************local variables declaration ends here*****************************************************/
-        int fdWrite,retValue;
-        char *buffer;
-        int blockSize, blockCount;
-	double TimeDifference;
-	clock_t start;
-	clock_t end;
-        /***************local variables declaration ends here*****************************************************/
+/***************local variables declaration ends here*****************************************************/
+  int fdWrite,retValue;
+  char *buffer;
+  int blockSize, blockCount;
+  double TimeDifference;
+  clock_t start;
+  clock_t end;
+/***************local variables declaration ends here*****************************************************/
         
-        /***************User Inputs for block_size and block count starts*****************************************/
-        printf("Enter the Block size\n");
-        scanf("%d",&blockSize);
-        printf("Enter the Block Count\n");
-        scanf("%d",&blockCount);
+/***************User Inputs for block_size and block count starts*****************************************/
+  printf("Enter the Block size\n");
+  scanf("%d",&blockSize);
+  printf("Enter the Block Count\n");
+  scanf("%d",&blockCount);
+  if( (buffer = (char *)malloc(blockSize*sizeof(char))) == NULL) {
+      printf("Error in Allocating desired buffer size\n");
+      return EXIT_FAILURE;
+  }
+/***************User Inputs for block_size and block count ends******************************************/
 
-        if ( (buffer = (char *)malloc(blockSize*sizeof(char))) == NULL){
-               	printf("Error in Allocating desired buffer size\n");
-                EXIT_FAILURE;
-        }
-        /***************User Inputs for block_size and block count ends******************************************/
-
-        if( (buffer  = calloc(blockSize,(sizeof(char)))) == NULL) {
-                printf("Memory not Allocated:error\n");
-                EXIT_FAILURE;
-        }
+  if( (buffer  = calloc(blockSize,(sizeof(char)))) == NULL) {
+       printf("Memory not Allocated:error\n");
+       return EXIT_FAILURE;
+  }        
         
-        if( (fdWrite = open(FILETOWRITE,MODE,PERMISSION)) == -1) {
-                printf("Error %s",strerror(errno));
-                EXIT_FAILURE;
-        }
-        /***************Writing buffer with size Blocksize given by user for Bockcount number of times**********/
-        start= clock();                  // starting the clock call to count the event
-        while( blockCount > 0) {
-                if( (retValue = write(fdWrite, buffer, blockSize)) == -1) {
-                     printf("Error:Write = %s\n",strerror(errno)); 
-                     EXIT_FAILURE;
-                }
-        	blockCount--;
-        }
-	end=clock();                   //clock event ends here
-	/***************Writing buffer operation ends here*****************************************************/
+  if( (fdWrite = open(FILETOWRITE,MODE,PERMISSION)) == -1) {
+       printf("Error %s",strerror(errno));
+       return EXIT_FAILURE;
+  }
+ /***************Writing buffer with size Blocksize given by user for Bockcount number of times**********/
+  start= clock();                  // starting the clock call to count the event
+  while(blockCount > 0) {
+    if( (retValue = write(fdWrite, buffer, blockSize)) == -1) {
+         printf("Error:Write = %s\n",strerror(errno)); 
+         return EXIT_FAILURE;
+    }
+    blockCount--;
+  }
+  end=clock();                   //clock event ends here
+/***************Writing buffer operation ends here*****************************************************/
 
-        timeDiff=(double)(end-start);   
-        timeDiff/=CLOCKS_PER_SEC;              // taking diff of operation for buff write in terms of clocks/sec
-        printf("Time taken for write cycle: %lf\n",timeDiff);
-        EXIT_SUCCESS;
+  timeDiff=(double)(end-start);   
+  timeDiff/=CLOCKS_PER_SEC;              // taking diff of operation for buff write in terms of clocks/sec
+  printf("Time taken for write cycle: %lf\n",timeDiff);
+  return EXIT_SUCCESS;
 }
